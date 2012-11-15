0bf87d3318fa16432064c0ace7ffd4276cadfa05
olivier briand <olivier.briand@orange.com>
2012-11-15 15:02:23 +0100

    Zoom and add a POI displayed in Alma, Rennes

--- src/com/obriand/android_maps/MainActivity.java
+++ src/com/obriand/android_maps/MainActivity.java
@@ -1,8 +1,15 @@
 package com.obriand.android_maps;
 
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 import android.view.Menu;
 
@@ -22,8 +29,24 @@ public class MainActivity extends MapActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		// Set the zoom feature
 		MapView mapView = (MapView) findViewById(R.id.main_mapview);
 	    mapView.setBuiltInZoomControls(true);
+	    
+	    //Set an overlay for POI
+	    List<Overlay> mapOverlays = mapView.getOverlays();
+	    Drawable drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+	    HelloItemizedOverlay itemizedoverlay = new HelloItemizedOverlay(drawable, this);
+	    
+	    // Create the POI
+	    float lat = 48.08232f;
+	    float lng = -1.67874f;
+	    GeoPoint point = new GeoPoint((int)(lat * 1E6), (int)(lng * 1E6));
+	    OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+	    
+	    // Add the POI to the overlay
+	    itemizedoverlay.addOverlay(overlayitem);
+	    mapOverlays.add(itemizedoverlay);
 	}
 
 	@Override
